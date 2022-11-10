7a5d64468bf9334d411a986ad520f89c034b6b0f
fereidani <info@fereidani.com>
2022-11-11 01:16:54 +0330

    add DropTester and tests for it. Vector tests + some cleanups

--- src/kanal_tests.rs
+++ src/kanal_tests.rs
@@ -3,13 +3,52 @@
 const MESSAGES: usize = 1000000;
 #[cfg(miri)]
 const MESSAGES: usize = 1024;
-
 const THREADS: usize = 64;
+
+use std::sync::{
+    atomic::{AtomicU64, Ordering},
+    Arc,
+};
+
+struct DropTester {
+    i: usize,
+    dropped: bool,
+    counter: Arc<AtomicU64>,
+}
+
+impl Drop for DropTester {
+    fn drop(&mut self) {
+        if self.dropped {
+            panic!("double dropped");
+        }
+        if self.i == 0 {
+            panic!("bug: i=0 is invalid value for drop tester");
+        }
+        self.dropped = true;
+        self.counter.fetch_add(1, Ordering::SeqCst);
+    }
+}
+
+impl DropTester {
+    #[allow(dead_code)]
+    fn new(counter: Arc<AtomicU64>, i: usize) -> Self {
+        if i == 0 {
+            panic!("don't initialize DropTester with 0");
+        }
+        return Self {
+            i,
+            dropped: false,
+            counter,
+        };
+    }
+}
 
 #[cfg(test)]
 mod tests {
-    use crate::kanal_tests::{MESSAGES, THREADS};
+    use crate::kanal_tests::{DropTester, MESSAGES, THREADS};
     use crate::{bounded, unbounded, Receiver, Sender};
+    use std::sync::atomic::{AtomicU64, Ordering};
+    use std::sync::Arc;
 
     fn new<T>(cap: Option<usize>) -> (Sender<T>, Receiver<T>) {
         match cap {
@@ -39,6 +78,31 @@ mod tests {
             }
         })
         .unwrap();
+    }
+
+    macro_rules! mpmc_dyn {
+        ($new:expr,$cap:expr) => {
+            let (tx, rx) = new($cap);
+
+            crossbeam::scope(|scope| {
+                for _ in 0..THREADS {
+                    scope.spawn(|_| {
+                        for _i in 0..MESSAGES / THREADS {
+                            tx.send($new).unwrap();
+                        }
+                    });
+                }
+
+                for _ in 0..THREADS {
+                    scope.spawn(|_| {
+                        for _ in 0..MESSAGES / THREADS {
+                            rx.recv().unwrap();
+                        }
+                    });
+                }
+            })
+            .unwrap();
+        };
     }
 
     fn mpsc(cap: Option<usize>) {
@@ -97,6 +161,18 @@ mod tests {
     }
 
     #[test]
+    fn drop_test() {
+        let counter = Arc::new(AtomicU64::new(0));
+        mpmc_dyn!(DropTester::new(counter.clone(), 10), Some(1));
+        assert_eq!(counter.load(Ordering::SeqCst), MESSAGES as u64);
+    }
+
+    #[test]
+    fn vec_test() {
+        mpmc_dyn!(vec![1, 2, 3], Some(1));
+    }
+
+    #[test]
     fn spsc_1() {
         spsc(Some(1));
     }
@@ -150,15 +226,47 @@ mod tests {
 #[cfg(feature = "async")]
 #[cfg(test)]
 mod async_tests {
-    use crate::kanal_tests::{MESSAGES, THREADS};
+    use crate::kanal_tests::{DropTester, MESSAGES, THREADS};
     use crate::{bounded_async, unbounded_async, AsyncReceiver, AsyncSender};
-    // Executor with constant config. no-size
+    use std::sync::atomic::{AtomicU64, Ordering};
+    use std::sync::Arc;
 
     fn new_async<T>(cap: Option<usize>) -> (AsyncSender<T>, AsyncReceiver<T>) {
         match cap {
             None => unbounded_async(),
             Some(cap) => bounded_async(cap),
         }
+    }
+
+    macro_rules! async_mpmc_dyn {
+        ($pre:stmt,$new:expr,$cap:expr) => {
+            let (tx, rx) = new_async($cap);
+            let mut list = Vec::new();
+            for _ in 0..THREADS {
+                let tx = tx.clone();
+                $pre
+                let h = tokio::spawn(async move {
+                    for _i in 0..MESSAGES / THREADS {
+                        tx.send($new).await.unwrap();
+                    }
+                });
+                list.push(h);
+            }
+
+            for _ in 0..THREADS {
+                let rx = rx.clone();
+                let h = tokio::spawn(async move {
+                    for _i in 0..MESSAGES / THREADS {
+                        rx.recv().await.unwrap();
+                    }
+                });
+                list.push(h);
+            }
+
+            for h in list {
+                h.await.unwrap();
+            }
+        };
     }
 
     async fn async_mpsc(cap: Option<usize>) {
@@ -237,6 +345,19 @@ mod async_tests {
             h.await.unwrap();
         }
     }
+
+    #[tokio::test]
+    async fn async_drop_test() {
+        let counter = Arc::new(AtomicU64::new(0));
+        async_mpmc_dyn!(let counter=counter.clone(),DropTester::new(counter.clone(), 10), Some(1));
+        assert_eq!(counter.load(Ordering::SeqCst), MESSAGES as u64);
+    }
+
+    #[tokio::test]
+    async fn async_vec_test() {
+        async_mpmc_dyn!({}, vec![1, 2, 3], Some(1));
+    }
+
     #[tokio::test]
     async fn async_one_msg() {
         let (s, r) = bounded_async::<u8>(1);
