7b4ea56251d29a00129fb1695af0f9dab4749673
fereidani <info@fereidani.com>
2023-01-16 16:54:56 +0330

    add integrity tests

--- tests/oneshot_test.rs
+++ tests/oneshot_test.rs
@@ -1,9 +1,32 @@
+mod utils;
 use std::time::Duration;
+use utils::*;
 
 // this test module uses the Box<usize> only to check for memory issues related to the drop impl with MIRI, it's inefficient to use a channel like this in production code.
 
 fn delay() {
     std::thread::sleep(Duration::from_millis(10));
+}
+
+macro_rules! integrity_test {
+    ($zero:expr,$ones:expr) => {
+        let (tx, rx) = kanal::oneshot();
+        crossbeam::scope(|scope| {
+            scope.spawn(|_| {
+                tx.send($zero).unwrap();
+            });
+            assert_eq!(rx.recv().unwrap(), $zero);
+        })
+        .unwrap();
+        let (tx, rx) = kanal::oneshot();
+        crossbeam::scope(|scope| {
+            scope.spawn(|_| {
+                tx.send($ones).unwrap();
+            });
+            assert_eq!(rx.recv().unwrap(), $ones);
+        })
+        .unwrap();
+    };
 }
 
 #[test]
@@ -77,9 +100,92 @@ fn drop_both() {
     }
 }
 
+#[test]
+fn integrity_u8() {
+    integrity_test!(0u8, !0u8);
+}
+
+#[test]
+fn integrity_u32() {
+    integrity_test!(0u32, !0u32);
+}
+
+#[test]
+fn integrity_u64() {
+    integrity_test!(0u64, !0u64);
+}
+
+#[test]
+fn integrity_box_u64() {
+    integrity_test!(Box::new(0u64), Box::new(!0u64));
+}
+
+#[test]
+fn integrity_big() {
+    integrity_test!((0u64, 0u64, 0u64, 0u64), (!0u64, !0u64, !0u64, !0u64));
+}
+
+#[test]
+fn integrity_string() {
+    integrity_test!("", "not empty");
+}
+
+#[test]
+fn integrity_big_tail() {
+    integrity_test!((0u64, 0u64, 0u64, 0u8), (!0u64, !0u64, !0u64, !0u8));
+}
+
+#[test]
+fn integrity_padded_rust() {
+    integrity_test!(
+        Padded {
+            a: false,
+            b: 0x0,
+            c: 0x0
+        },
+        Padded {
+            a: true,
+            b: 0xFF,
+            c: 0xFFFFFFFF
+        }
+    );
+}
+
+#[test]
+fn integrity_padded_c() {
+    integrity_test!(
+        PaddedReprC {
+            a: false,
+            b: 0x0,
+            c: 0x0
+        },
+        PaddedReprC {
+            a: true,
+            b: 0xFF,
+            c: 0xFFFFFFFF
+        }
+    );
+}
+
 #[cfg(feature = "async")]
 mod asyncs {
+    use super::*;
     use std::time::Duration;
+
+    macro_rules! integrity_test {
+        ($zero:expr,$ones:expr) => {
+            let (tx, rx) = kanal::oneshot_async();
+            tokio::spawn(async move {
+                tx.send($zero).await.unwrap();
+            });
+            assert_eq!(rx.recv().await.unwrap(), $zero);
+            let (tx, rx) = kanal::oneshot_async();
+            tokio::spawn(async move {
+                tx.send($ones).await.unwrap();
+            });
+            assert_eq!(rx.recv().await.unwrap(), $ones);
+        };
+    }
 
     async fn delay() {
         tokio::time::sleep(Duration::from_millis(10)).await;
@@ -156,5 +262,72 @@ mod asyncs {
             drop(s);
             h.await.unwrap();
         }
+    }
+
+    #[tokio::test]
+    async fn integrity_u8() {
+        integrity_test!(0u8, !0u8);
+    }
+
+    #[tokio::test]
+    async fn integrity_u32() {
+        integrity_test!(0u32, !0u32);
+    }
+
+    #[tokio::test]
+    async fn integrity_u64() {
+        integrity_test!(0u64, !0u64);
+    }
+
+    #[tokio::test]
+    async fn integrity_box_u64() {
+        integrity_test!(Box::new(0u64), Box::new(!0u64));
+    }
+
+    #[tokio::test]
+    async fn integrity_big() {
+        integrity_test!((0u64, 0u64, 0u64, 0u64), (!0u64, !0u64, !0u64, !0u64));
+    }
+
+    #[tokio::test]
+    async fn integrity_string() {
+        integrity_test!("", "not empty");
+    }
+
+    #[tokio::test]
+    async fn integrity_big_tail() {
+        integrity_test!((0u64, 0u64, 0u64, 0u8), (!0u64, !0u64, !0u64, !0u8));
+    }
+
+    #[tokio::test]
+    async fn integrity_padded_rust() {
+        integrity_test!(
+            Padded {
+                a: false,
+                b: 0x0,
+                c: 0x0
+            },
+            Padded {
+                a: true,
+                b: 0xFF,
+                c: 0xFFFFFFFF
+            }
+        );
+    }
+
+    #[tokio::test]
+    async fn integrity_padded_c() {
+        integrity_test!(
+            PaddedReprC {
+                a: false,
+                b: 0x0,
+                c: 0x0
+            },
+            PaddedReprC {
+                a: true,
+                b: 0xFF,
+                c: 0xFFFFFFFF
+            }
+        );
     }
 }
