a56684afa45d5079edef398862e9c55210a752a6
Khashayar Fereidani <info@fereidani.com>
2026-07-19 17:19:27 +0000

    ci: run loom model tests in pre_push script

--- pre_push.sh
+++ pre_push.sh
@@ -8,3 +8,6 @@ cargo check --no-default-features
 cargo check --features std-mutex
 cargo check --all-features
 cargo test --all-features
+# model-check the channel protocol with loom (release: exhaustive
+# interleaving exploration is CPU-bound)
+RUSTFLAGS="--cfg loom" cargo test --release --all-features --lib --test loom
